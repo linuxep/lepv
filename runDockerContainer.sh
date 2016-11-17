3c14215f925ae80f3038fc89c484569866cdaac0
Mac Xu <mxu@marinsoftware.com>
2016-11-17 08:46:38 +0800

    fix the script of running Docker container.

--- runDockerContainer.sh
+++ runDockerContainer.sh
@@ -3,8 +3,8 @@
 echo "running container:"
 docker ps
 
-echo "stop and remove existing lepv container"
-docker ps -a | awk '{ print $1,$2 }' | grep linuxep/lepv0.1 | awk '{print $1 }' | xargs -I {} docker stop {} | xargs -I {} docker rm {}
+echo "stop and remove existing LEPV container"
+docker ps -a | grep 0.0.0.0:8889 | awk '{print $1}' | xargs -I {} docker stop {} | xargs -I {} docker rm {}
 
 echo "Run LEPV in container"
 docker run -t -p 8889:8889 linuxep/lepv0.1
