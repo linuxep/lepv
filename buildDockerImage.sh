f19a0d91fca5c5b2a9d6c005eef4d8f1987adf9c
zhouyuyan <joy.zhou@honeywell.com>
2016-12-21 11:59:32 +0800

    addmirror

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+echo "add Mainland Registry Mirror"
+#echo "DOCKER_OPTS=\" --registry-mirror=http://3d9b08b6.m.daocloud.io\"" | sudo tee -a /etc/default/docker 
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://3d9b08b6.m.daocloud.io
+sudo service docker restart
+
 echo "Building docker image"
 docker build -f docker/Dockerfile -t linuxep/lepv0.1 .
