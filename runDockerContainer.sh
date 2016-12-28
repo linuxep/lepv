eaf69daf1a6b5ec475e6121cbae5ff345d591aff
Mac Xu <mxu@marinsoftware.com>
2016-12-28 11:18:12 +0800

    refactor the script of launching docker container for LEPV.
    use "net=host" for Linux
    use port mapping "8889:8889" for MacOS because Docker engine runs in a HyperKit VM on MacOS.

--- runDockerContainer.sh
+++ runDockerContainer.sh
@@ -4,8 +4,21 @@ echo "running container:"
 docker ps
 
 echo "stop and remove existing LEPV container"
+
+# remove by port
 docker ps -a | grep 0.0.0.0:8889 | awk '{print $1}' | xargs -I {} docker stop {} | xargs -I {} docker rm {}
 
+# remove by image name
+docker ps -a | grep lepv | awk '{print $1}' | xargs -I {} docker stop {} | xargs -I {} docker rm {}
+
 echo "Run LEPV in container"
-#docker run -t -p 8889:8889 linuxep/lepv0.1
-docker run -t --net="host" linuxep/lepv0.1
+
+os=$( uname )
+echo "OS: $os"
+if [ "$os" == "Darwin" ]; then
+    echo "Running Docker container on MacOS"
+    docker run -t -p 8889:8889 linuxep/lepv0.1
+else
+    echo "Running Docker container on Linux"
+    docker run -t --net="host" linuxep/lepv0.1
+fi
