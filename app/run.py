442cd2a0baf851d0587c2515bacd9e8a8383bd2a
Mac Xu <mxu@marinsoftware.com>
2017-08-27 12:58:02 +0800

    add an API endpoint.

--- app/run.py
+++ app/run.py
@@ -8,5 +8,11 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/command/<cmd>')
+def sendRawCommand(cmd):
+   return 'Sending raw command: %s!' % cmd
+
+
+
 if __name__ == '__main__':
     app.run()
