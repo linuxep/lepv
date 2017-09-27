90659045fd0cf86287baa72450a5853dd9f4c805
Mac Xu <mxu@marinsoftware.com>
2017-09-27 11:36:32 +0800

    CPU api done, waiting to be tested.

--- app/modules/cpu/views.py
+++ app/modules/cpu/views.py
@@ -9,10 +9,45 @@ from app.modules.cpu.CPUProfiler import CPUProfiler
 cpuAPI = Blueprint('cpuAPI', __name__, url_prefix='/api/cpu')
 
 
+@cpuAPI.route('/capacity/<server>', methods=['GET'])
+def getCpuCapacity(server):
+
+    profiler = CPUProfiler(server)
+    data = profiler.getCapacity()
+
+    return jsonify(data)
+
 @cpuAPI.route('/count/<server>', methods=['GET'])
 def getCpuCount(server):
 
     profiler = CPUProfiler(server)
     data = profiler.getProcessorCount()
+
+    return jsonify(data)
+
+
+@cpuAPI.route('/status/<server>', methods=['GET'])
+def getCpuStatus(server):
+
+    profiler = CPUProfiler(server)
+    data = profiler.getStatus()
+
+    return jsonify(data)
+
+
+@cpuAPI.route('/stat/<server>', methods=['GET'])
+def getCpuStat(server):
+
+    profiler = CPUProfiler(server)
+    data = profiler.getStat()
+
+    return jsonify(data)
+
+
+@cpuAPI.route('/top/<server>', methods=['GET'])
+def getCpuTop(server):
+
+    profiler = CPUProfiler(server)
+    data = profiler.getTopOutput()
 
     return jsonify(data)
