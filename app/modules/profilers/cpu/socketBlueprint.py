46c59051b421931f11e549519439fb23b88f1ddc
Mac Xu <mxu@marinsoftware.com>
2017-11-02 20:20:21 +0800

    set up socketIO on server side for CPU.

--- app/modules/profilers/cpu/socketBlueprint.py
+++ app/modules/profilers/cpu/socketBlueprint.py
@@ -6,7 +6,7 @@ cpu_blueprint = SocketIOBlueprint('')
 
 
 @cpu_blueprint.on('cpu.stat.req')
-def get_avg_load(request):
+def get_cpu_stat(request):
     print('received cpu.stat.req: ' + request['server'])
 
     server = request['server']
@@ -15,3 +15,35 @@ def get_avg_load(request):
 
     emit('cpu.stat.res',  data)
 
+
+@cpu_blueprint.on('cpu.status.req')
+def get_cpu_status(request):
+    print('received cpu.status.req: ' + request['server'])
+
+    server = request['server']
+    profiler = CPUProfiler(server)
+    data = profiler.getStatus()
+
+    emit('cpu.status.res',  data)
+
+
+@cpu_blueprint.on('cpu.avgload.req')
+def get_avg_load(request):
+    print('received cpu.avgload.req: ' + request['server'])
+
+    server = request['server']
+    profiler = CPUProfiler(server)
+    data = profiler.get_average_load()
+
+    emit('cpu.avgload.res',  data)
+
+
+@cpu_blueprint.on('cpu.top.req')
+def get_top(request):
+    print('received cpu.top.req: ' + request['server'])
+
+    server = request['server']
+    profiler = CPUProfiler(server)
+    data = profiler.getTopOutput()
+
+    emit('cpu.top.res',  data)
