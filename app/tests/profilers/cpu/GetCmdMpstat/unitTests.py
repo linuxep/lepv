90c7afcaa9c051fc1b7c19860b9f967a3acb3c3f
Mac Xu <mxu@marinsoftware.com>
2017-11-04 22:33:25 +0800

    more changes for tests, it's getting closer to the JSON driven I'd wanted.

--- app/tests/profilers/cpu/GetCmdMpstat/unitTests.py
+++ app/tests/profilers/cpu/GetCmdMpstat/unitTests.py
@@ -1,31 +1,26 @@
 """Tests for CPU profiler method: GetProcLoadavg"""
+from tests.profilers.cpu.lepvTestCase import LepvTestCase
+
 __author__    = "Copyright (c) 2017, LEP>"
 __copyright__ = "Licensed under GPLv2 or later."
 
 import unittest
-from ddt import ddt, data, file_data, unpack
+from ddt import ddt, file_data
 from pprint import pprint
 
 from modules.profilers.cpu.CPUProfiler import CPUProfiler
 
 @ddt
-class GetCmdMpstatTestCase(unittest.TestCase):
+class GetCmdMpstatTestCase(LepvTestCase):
 
     def setUp(self):
         self.profiler = CPUProfiler('')
-
-    def describe(self, kernel, os, cpu, note):
-        print(kernel)
-        print(os)
-        print(cpu)
-        print(note)
-
 
     @file_data("unittests.json")
     def test(self, kernel, os, cpu, note, lepdResult, expected, expectedMatchType):
         self.describe(kernel, os, cpu, note)
 
-        print(lepdResult)
+        actual = self.profiler.get_stat()
         print(expectedMatchType)
         pprint(expected)
 
---
+++ app/tests/profilers/cpu/lepvTestCase.py
@@ -0,0 +1,14 @@
+__author__    = "Copyright (c) 2017, LEP>"
+__copyright__ = "Licensed under GPLv2 or later."
+
+import unittest
+
+
+class LepvTestCase(unittest.TestCase):
+
+    def describe(self, kernel, os, cpu, note):
+        print(kernel)
+        print(os)
+        print(cpu)
+        print(note)
+
