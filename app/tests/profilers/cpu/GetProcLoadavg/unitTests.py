ce63625e3be3d219320466b86cb467b15687ba39
Mac Xu <mxu@marinsoftware.com>
2017-11-08 21:24:25 +0800

    unit tests for avg load.

--- app/tests/profilers/cpu/GetProcLoadavg/unitTests.py
+++ app/tests/profilers/cpu/GetProcLoadavg/unitTests.py
@@ -1,84 +1,46 @@
 """Tests for CPU profiler method: GetProcLoadavg"""
+from modules.utils.dictUtil import DictUtil
+from tests.profilers.lepvTestCase import LepvTestCase
+
 __author__    = "Copyright (c) 2017, LEP>"
 __copyright__ = "Licensed under GPLv2 or later."
 
 import unittest
+from ddt import ddt, file_data
+from pprint import pprint
 
 from modules.profilers.cpu.CPUProfiler import CPUProfiler
 
-
-class GetProcLoadavgTestCase(unittest.TestCase):
+@ddt
+class GetProcLoadavgTestCase(LepvTestCase):
 
     def setUp(self):
         self.profiler = CPUProfiler('')
 
-    def describe(self, test_data):
-        for test_data_key in test_data:
-            if test_data_key == 'lepdResult':
-                continue
-            if test_data_key == 'expected':
-                continue
+    def validateProcLoadavg(self, expected, actual, expectedMatchType):
+        print("Expected:")
+        pprint(expected)
 
-            print(test_data_key + ": " + test_data[test_data_key])
+        print("Actual:")
+        pprint(actual)
 
-    def doPositiveTest(self, test_data):
-        self.describe(test_data)
+        compare_result = DictUtil.compare(actual, expected)
 
-        actual_result = self.profiler.get_average_load(test_data['lepdResult'])
-
-        # self.assertTrue(isinstance(count, int), "CPU processor count '%s' is not an integer" % count)
-        self.assertTrue(actual_result, "Result should NOT be null")
-
-        self.assertIn('data', actual_result, "field 'data' should present")
-
-        actualData = actual_result['data']
-        self.assertIn('last1', actualData, "'last1' should present in 'data'")
-        self.assertEqual(str(test_data['expected']['data']['last1']), str(actualData['last1']), "'last1' values do NOT match!")
-
-        self.assertIn('last5', actualData, "'last5' should present in 'data'")
-        self.assertEqual(str(test_data['expected']['data']['last5']), str(actualData['last5']), "'last5' values do NOT match!")
-
-        self.assertIn('last15', actualData, "'last15' should present in 'data'")
-        self.assertEqual(str(test_data['expected']['data']['last15']), str(actualData['last15']), "'last15' values do NOT match!")
-
-    def testGoldenPass(self):
-
-        test_data = {
-          "kernel": "",
-          "os": "linux",
-          "cpu": "x86",
-          "note": "data from www.rmlink.cn",
-          "lepdResult":	"0.18 0.19 0.12 1/77 11132\nlepdendstring",
-          "expected": {
-            "data": {
-              "last1": 0.18,
-              "last5": 0.19,
-              "last15": 0.12
-            }
-          }
-        }
-
-        self.doPositiveTest(test_data)
+        if expectedMatchType == 'equals':
+            self.assertEqual(compare_result, 0, "Expected and Actual does not match")
+        elif expectedMatchType == 'contains':
+            self.assertIn(compare_result, [0, 1], "Actual does not contain the expected")
+        else:
+            print("")
 
 
-    def testGoldenPassForX86(self):
+    @file_data("unittests.json")
+    def test(self, kernel, os, cpu, note, lepdResult, expected, expectedMatchType):
+        self.describe(kernel, os, cpu, note)
 
-        test_data = {
-          "kernel": "",
-          "os": "linux",
-          "cpu": "x86",
-          "note": "data from www.rmlink.cn",
-          "lepdResult":	"0.18 0.79 0.92 1/77 11132\nlepdendstring",
-          "expected": {
-            "data": {
-              "last1": 0.18,
-              "last5": 0.79,
-              "last15": 0.92
-            }
-          }
-        }
+        actual = self.profiler.get_average_load(lepdResult)
+        self.validateProcLoadavg(expected, actual, expectedMatchType)
 
-        self.doPositiveTest(test_data)
 
 if( __name__ =='__main__' ):
     unittest.main()
