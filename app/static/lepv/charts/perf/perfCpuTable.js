b8401fc2345486a9fa9d436fd2bac6ff12ba342b
chowyu08 <chowyu08@gmail.com>
2017-11-12 09:54:39 +0800

    perf cpu add commit

---
+++ app/static/lepv/charts/perf/perfCpuTable.js
@@ -0,0 +1,92 @@
+/*
+ * Open source under the GPLv2 License or later.
+ * Copyright (c) 2016, Mac Xu <shinyxxn@hotmail.com>.
+ */
+
+var PerfCpuTable = function(rootDivName, socket, server) {
+
+    LepvChart.call(this, rootDivName, socket, server);
+
+    this.socket_message_key = 'perf.cpuclock';
+    
+    this.setTableDivName(rootDivName);
+    
+    this.chartTitle = "Perf Table";
+    this.chartHeaderColor = 'blue';
+    
+    this.maxDataCount = 25;
+    this.refreshInterval = 5;
+
+    this.initializeChart();
+    this.setupSocketIO();
+};
+
+PerfCpuTable.prototype = Object.create(LepvChart.prototype);
+PerfCpuTable.prototype.constructor = PerfCpuTable;
+
+PerfCpuTable.prototype.initializeChart = function() {
+    this.table = $(this.tableDivName).DataTable( {
+        destroy: true,
+        paging: false,
+        info: false,
+        searching: true,
+        columns: [
+            {
+                title: "Command",
+                orderable: false
+            },
+            {
+                title: "Overhead",
+                orderable: false
+            },
+            {
+                title: "Shared Object",
+                orderable: false
+            },
+            {
+                title: "Symbol",
+                orderable: false
+            }
+        ],
+        order: [[ 1, "desc" ]]
+    });
+};
+
+PerfCpuTable.prototype.updateChartData = function(response) {
+    // console.log(response)
+    data = response['data']
+    // console.log(data)
+    var thisChart = this;
+    
+    this.table.rows().remove().draw( true );
+    if (data != null) {
+        $.each( data, function( itemIndex, dataItem ) {
+
+            if (itemIndex >= thisChart.maxDataCount) {
+                return;
+            }
+
+            thisChart.table.row.add([
+                dataItem['Command'],
+                dataItem['Overhead'],
+                dataItem['Shared Object'],
+                dataItem['Symbol']
+            ]);
+            index = index + 1;
+        });
+    } else {
+        var index = 0;
+        while(index < thisChart.maxDataCount) {
+            thisChart.table.row.add([
+                "--",
+                "--",
+                "--",
+                "--"
+            ]);
+            index = index + 1;
+        }
+    }
+    this.table.draw(true);
+    
+    this.requestData();
+};
