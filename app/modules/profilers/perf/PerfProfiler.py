c41dce416c614cea08bed879ae20a3f564d0c334
Mac Xu <mxu@marinsoftware.com>
2017-11-27 22:26:43 +0800

    draft version of parsing logic for GetCmdPerfFlame

--- app/modules/profilers/perf/PerfProfiler.py
+++ app/modules/profilers/perf/PerfProfiler.py
@@ -14,6 +14,64 @@ class PerfProfiler:
         
         self.dataCount = 25
 
+
+    def parse_perf_block(self, block_lines):
+        first_line = block_lines.pop(0)
+        # first line is like this:
+        # swapper     0 [000] 1212828.569239:   10101010 cpu-clock:
+        header_line_entities = first_line.split()
+
+        perf_block = {
+            'AAA': header_line_entities[0],
+            'BBB': header_line_entities[1],
+            'CCC': header_line_entities[2],
+            'DDD': header_line_entities[3],
+            'EEE': header_line_entities[4],
+            'FFF': header_line_entities[5],
+
+            'children': []
+        }
+
+        for child_line in block_lines:
+            child_entities = child_line.split()
+            perf_block['children'].append({
+                'uuid': child_entities[0],
+                'process_name': child_entities[1],
+                'symbol': child_entities[2]
+            })
+
+        return perf_block
+
+    def get_cmd_perf_flame(self, response_lines=None):
+
+        lepd_command = 'GetCmdPerfFlame'
+
+        if not response_lines:
+            response_lines = self.client.getResponse(lepd_command)
+        elif isinstance(response_lines, str):
+            response_lines = self.client.split_to_lines(response_lines)
+
+        if len(response_lines) == 0:
+            return {}
+
+        # for line in response_lines:
+        #     print(line)
+
+        parsed_blocks = []
+
+        block_lines = []
+        for line in response_lines:
+            # each block is separated by an empty line
+            if line.strip():
+                block_lines.append(line)
+                continue
+
+            parsed_block = self.parse_perf_block(block_lines)
+            parsed_blocks.append(parsed_block)
+            block_lines = []
+
+        return parsed_blocks
+
     def get_perf_cpu_clock(self, response_lines=None):
 
         lepd_command = 'GetCmdPerfCpuclock'
@@ -73,7 +131,7 @@ if __name__ == '__main__' :
     profiler = PerfProfiler(server='www.rmlink.cn', config='debug')
 
     pp = pprint.PrettyPrinter(indent=2)
-    
-    responseData = profiler.get_perf_cpu_clock()
+
+    responseData = profiler.get_cmd_perf_flame()
     pp.pprint(responseData)
 
