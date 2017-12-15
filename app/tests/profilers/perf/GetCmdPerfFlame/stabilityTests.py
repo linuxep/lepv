0450edc0e871902834c642a6abafdbca2873b0c7
Mac Xu <mxu@marinsoftware.com>
2017-12-15 16:46:39 +0800

    add stabilityTests for all profilers.

---
+++ app/tests/profilers/perf/GetCmdPerfFlame/stabilityTests.py
@@ -0,0 +1,25 @@
+from tests.profilers.lepvTestCase import LepvTestCase
+
+__author__    = "Copyright (c) 2017, LEP>"
+__copyright__ = "Licensed under GPLv2 or later."
+
+import unittest
+
+from modules.profilers.perf.PerfProfiler import PerfProfiler
+
+
+class StabilityTestCase(LepvTestCase):
+
+    def setUp(self):
+        pass
+
+    def validate(self, actual):
+        pass
+
+    def test_stability(self):
+        profiler = PerfProfiler('www.rmlink.cn')
+        self.repeated_test(profiler.get_cmd_perf_flame)
+
+
+if __name__ =='__main__':
+    unittest.main()
