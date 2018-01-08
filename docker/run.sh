6ae49817c72ac5dbe4c15baf8090864445a5600c
zhouyuyan <joy.zhou@honeywell.com>
2018-01-08 13:03:21 +0800

    modify

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 service nginx start 
-gunicorn -k gevent -b 0.0.0.0:8000 -t 30 --log-level=DEBUG run:app
+#gunicorn -k gevent -b 0.0.0.0:8000 -t 30 -w 4 --log-level=DEBUG run:app
+gunicorn -c gun.conf run:app
