7159601664ee400bf5ba4597e7b86a535e4fdae0
Mac Xu <mxu@marinsoftware.com>
2018-01-12 21:49:01 +0800

    first component seems to work

--- app/static/js/vue/chart-card.js
+++ app/static/js/vue/chart-card.js
@@ -1,11 +1,20 @@
 
 Vue.component('lepv-chart-card', {
+
+  delimiters: ['[[',']]'],
+
+  props: [
+    'chart_id',
+    'header',
+    'icon'
+  ],
+
   template: `
 
   <div class="col-md-12">
-    <div id='container-div-IOChartsXXX' class="card mb-3">
+    <div :id='[[chart_id]]' class="card mb-3">
         <div class="card-header">
-            <i class="icon-syncalt"></i> XXXXXXXXXXXXXXXX -[GetCmdIostat]
+            <i :class="[[icon]]"></i> [[header]]
         </div>
         <div class="card-body"><div class="chart-panel"></div></div>
         <div class="card-footer small text-muted" hidden><i class="fa fa-bell" aria-hidden="true"> </i></div>
