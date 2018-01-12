7514124724d5f0ed00683fd9c9edac3d233f1fb6
Mac Xu <mxu@marinsoftware.com>
2018-01-12 21:32:43 +0800

    draft version of a Vue component

---
+++ app/static/js/vue/chart-card.js
@@ -0,0 +1,16 @@
+
+Vue.component('lepv-chart-card', {
+  template: `
+
+  <div class="col-md-12">
+    <div id='container-div-IOChartsXXX' class="card mb-3">
+        <div class="card-header">
+            <i class="icon-syncalt"></i> XXXXXXXXXXXXXXXX -[GetCmdIostat]
+        </div>
+        <div class="card-body"><div class="chart-panel"></div></div>
+        <div class="card-footer small text-muted" hidden><i class="fa fa-bell" aria-hidden="true"> </i></div>
+    </div>
+  </div>
+
+  `
+})
