46981d5ae14c8fc095a93da8a70b8c4784ba6f10
lixusheng <1530830954@qq.com>
2018-04-21 20:07:08 +0800

    解析数据

--- dataStore/modules/cpu/cpuPullAndStore.py
+++ dataStore/modules/cpu/cpuPullAndStore.py
@@ -3,7 +3,7 @@ __copyright__ = "Licensed under GPLv2 or later."
 
 
 from dataStore.lepdClient.LepdClient import LepdClient
-from dataStore.influxDbUtil.dbUtil import myInfluxDbClient
+from dataStore.influxDbUtil.dbUtil import MyInfluxDbClient
 
 import time
 
@@ -13,6 +13,14 @@ import time
 '''
 def pullAndStoreGetProcCpuinfo(lepdClient,influxDbClient):
     res = lepdClient.sendRequest('GetProcCpuinfo')
+    mystr = res['result'].split('\n')
+    data = {}
+    for x in mystr:
+        mylist=x.split('\t:')
+        if(len(mylist)==2):
+            data[mylist[0]]=mylist[1]
+    for key in data:
+        print(key+':'+data[key])
 
     json_body = [
         {
@@ -28,11 +36,11 @@ def pullAndStoreGetProcCpuinfo(lepdClient,influxDbClient):
         }
     ]
 
-    influxDbClient.write_points('www.rmlink.cn', json_body)
+    # influxDbClient.write_points('localhost', json_body)
 
 
 if(__name__=='__main__'):
-    lepdClient = LepdClient('www.rmlink.cn')
-    influxDbClient = myInfluxDbClient('localhost')
+    lepdClient = LepdClient('localhost')
+    influxDbClient = MyInfluxDbClient('localhost')
 
     pullAndStoreGetProcCpuinfo(lepdClient,influxDbClient)
--- dataStore/modules/memory/memoryPullAndStore.py
+++ dataStore/modules/memory/memoryPullAndStore.py
@@ -1,20 +1,23 @@
-__author__ = "李旭升 <programmerli@foxmail.com>"
+__author__ = "李旭升 programmerli@foxmail.com > "
+
 __copyright__ = "Licensed under GPLv2 or later."
+
+
+'''
+用lepdClient请求数据并把返回的数据用influxDbClient存储到InfluxDB中
+'''
 
 from dataStore.lepdClient.LepdClient import LepdClient
 from dataStore.influxDbUtil.dbUtil import MyInfluxDbClient
 
 import time
 
-'''
-用lepdClient请求数据并把返回的数据用influxDbClient存储到InfluxDB中
-'''
 def pullAndStoreGetProcMeminfo(lepdClient, influxDbClient):
     res = lepdClient.sendRequest('GetProcMeminfo')
     print(res)
-    str = res['result'].split('\n')
+    mystr = res['result'].split('\n')
     data = {}
-    for x in str:
+    for x in mystr:
 
         x1 = x.replace('kB', '')
         x2 = x1.replace(' ', '')
@@ -22,7 +25,8 @@ def pullAndStoreGetProcMeminfo(lepdClient, influxDbClient):
         if (len(list) == 2):
             data[list[0]] = list[1]
 
-    # print(data)
+
+    #print(data['Active'])
 
     json_body = [
         {
@@ -33,54 +37,54 @@ def pullAndStoreGetProcMeminfo(lepdClient, influxDbClient):
             },
             # "time": "2017-03-12T22:00:00Z",
             "fields": {
-                "MemTotal": data['MemTotal'],
-                "MemFree": data['MemFree'],
+                "MemTotal": int(data['MemTotal']),
+                "MemFree": int(data['MemFree']),
                 #   "MemAvailable": data['MemAvailable'],
-                "Buffers": data['Buffers'],
-                "Cached": data['Cached'],
-                "SwapCached": data['MemTotal'],
-                "Active": data['Active'],
-                "Inactive": data['Inactive'],
-                "Active(anon)": data['Active(anon)'],
-                "Inactive(anon)": data['Inactive(anon)'],
-                "Active(file)": data['Active(file)'],
-                "Inactive(file)": data['Inactive(file)'],
-                "Unevictable": data['Unevictable'],
-                "Mlocked": data['Mlocked'],
-                "SwapTotal": data['SwapTotal'],
-                "SwapFree": data['SwapFree'],
-                "Dirty": data['Dirty'],
-                "Writeback": data['Writeback'],
-                "AnonPages": data['AnonPages'],
-                "Mapped": data['Mapped'],
-                "Shmem": data['Shmem'],
-                "Slab": data['Slab'],
-                "SReclaimable": data['SReclaimable'],
-                "SUnreclaim": data['SUnreclaim'],
-                "KernelStack": data['KernelStack'],
-                "PageTables": data['PageTables'],
-                "NFS_Unstable": data['NFS_Unstable'],
-                "Bounce": data['Bounce'],
-                "WritebackTmp": data['WritebackTmp'],
-                "CommitLimit": data['CommitLimit'],
-                "Committed_AS": data['Committed_AS'],
-                "VmallocTotal": data['VmallocTotal'],
-                "VmallocUsed": data['VmallocUsed'],
-                "VmallocChunk": data['VmallocChunk'],
-                "HardwareCorrupted": data['HardwareCorrupted'],
-                "AnonHugePages": data['AnonHugePages'],
+                "Buffers": int(data['Buffers']),
+                "Cached": int(data['Cached']),
+                "SwapCached": int(data['MemTotal']),
+                "Active": int(data['Active']),
+                "Inactive": int(data['Inactive']),
+                "Active(anon)": int(data['Active(anon)']),
+                "Inactive(anon)": int(data['Inactive(anon)']),
+                "Active(file)": int(data['Active(file)']),
+                "Inactive(file)": int(data['Inactive(file)']),
+                "Unevictable": int(data['Unevictable']),
+                "Mlocked": int(data['Mlocked']),
+                "SwapTotal": int(data['SwapTotal']),
+                "SwapFree": int(data['SwapFree']),
+                "Dirty": int(data['Dirty']),
+                "Writeback": int(data['Writeback']),
+                "AnonPages": int(data['AnonPages']),
+                "Mapped": int(data['Mapped']),
+                "Shmem": int(data['Shmem']),
+                "Slab": int(data['Slab']),
+                "SReclaimable": int(data['SReclaimable']),
+                "SUnreclaim": int(data['SUnreclaim']),
+                "KernelStack": int(data['KernelStack']),
+                "PageTables": int(data['PageTables']),
+                "NFS_Unstable": int(data['NFS_Unstable']),
+                "Bounce": int(data['Bounce']),
+                "WritebackTmp": int(data['WritebackTmp']),
+                "CommitLimit": int(data['CommitLimit']),
+                "Committed_AS": int(data['Committed_AS']),
+                "VmallocTotal": int(data['VmallocTotal']),
+                "VmallocUsed": int(data['VmallocUsed']),
+                "VmallocChunk": int(data['VmallocChunk']),
+                "HardwareCorrupted": int(data['HardwareCorrupted']),
+                "AnonHugePages": int(data['AnonHugePages']),
                 # "ShmemHugePages": data['ShmemHugePages'],
                 # "ShmemPmdMapped": data['ShmemPmdMapped'],
                 # "CmaTotal": data['CmaTotal'],
                 # "CmaFree": data['CmaFree'],
-                "HugePages_Total": data['HugePages_Total'],
-                "HugePages_Free": data['HugePages_Free'],
-                "HugePages_Rsvd": data['HugePages_Rsvd'],
-                "HugePages_Surp": data['HugePages_Surp'],
-                "Hugepagesize": data['Hugepagesize'],
-                "DirectMap4k": data['DirectMap4k'],
-                "DirectMap2M": data['DirectMap2M'],
-                "DirectMap1G": data['DirectMap1G']
+                "HugePages_Total": int(data['HugePages_Total']),
+                "HugePages_Free": int(data['HugePages_Free']),
+                "HugePages_Rsvd": int(data['HugePages_Rsvd']),
+                "HugePages_Surp": int(data['HugePages_Surp']),
+                "Hugepagesize": int(data['Hugepagesize']),
+                "DirectMap4k": int(data['DirectMap4k']),
+                "DirectMap2M": int(data['DirectMap2M']),
+                "DirectMap1G": int(data['DirectMap1G'])
             }
         }
     ]
@@ -91,6 +95,6 @@ def pullAndStoreGetProcMeminfo(lepdClient, influxDbClient):
 if (__name__ == '__main__'):
     lepdClient = LepdClient('localhost')
     influxDbClient = MyInfluxDbClient('localhost')
-    for i in range(1):
+    for i in range(60):
         pullAndStoreGetProcMeminfo(lepdClient, influxDbClient)
         time.sleep(1)
