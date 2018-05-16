d2892b9f8be8833e9ab1135f7bf8b16937590c0b
lixusheng <1530830954@qq.com>
2018-05-16 18:51:13 +0800

    完成存储

--- dataStore/modules/pullAndStoreGetCmdIrqInfo.py
+++ dataStore/modules/pullAndStoreGetCmdIrqInfo.py
@@ -13,23 +13,32 @@ store the returned data into the influxDB by influxDBClient.
 def pullAndStoreGetCmdIrqInfo(lepdClient, influxDbClient):
     res = lepdClient.sendRequest('GetCmdIrqInfo')
     print(res)
-    # json_body = [
-    #     {
-    #         "measurement": "GetProcSwaps",
-    #         "tags": {
-    #             # the address of lepd
-    #             "server": lepdClient.server
-    #         },
-    #         # "time": "2017-03-12T22:00:00Z",
-    #         "fields": {
-    #             "LinuxVersion": mystr,
-    #             "compact_stall": int(data['compact_stall']),
-    #             "balloon_migrate": int(data['balloon_migrate']),
-    #         }
-    #     }
-    # ]
-    #
-    # influxDbClient.write_points(json_body)
+    str1 = res["result"].split(" ")
+    x1=str1[0].split(":")
+    x2=x1[1].split("/")
+    x3=x2[0]
+    print(x3)
+    y1=str1[1].split(":")
+    y2=y1[1].split("/")
+    y3=y2[0]
+    print(y3)
+
+    json_body = [
+        {
+            "measurement": "GetCmdIrqInfo",
+            "tags": {
+                # the address of lepd
+                "server": lepdClient.server
+            },
+            # "time": "2017-03-12T22:00:00Z",
+            "fields": {
+                "irq": int(x3),
+                "softirq": int(y3)
+            }
+        }
+    ]
+
+    influxDbClient.write_points(json_body)
 
 
 
--- dataStore/modules/pullAndStoreGetCmdMpstat-I.py
+++ dataStore/modules/pullAndStoreGetCmdMpstat-I.py
@@ -13,23 +13,40 @@ store the returned data into the influxDB by influxDBClient.
 def pullAndStoreGetCmdMpstat_I(lepdClient, influxDbClient):
     res = lepdClient.sendRequest('GetCmdMpstat-I')
     print(res)
-    # json_body = [
-    #     {
-    #         "measurement": "GetProcSwaps",
-    #         "tags": {
-    #             # the address of lepd
-    #             "server": lepdClient.server
-    #         },
-    #         # "time": "2017-03-12T22:00:00Z",
-    #         "fields": {
-    #             "LinuxVersion": mystr,
-    #             "compact_stall": int(data['compact_stall']),
-    #             "balloon_migrate": int(data['balloon_migrate']),
-    #         }
-    #     }
-    # ]
-    #
-    # influxDbClient.write_points(json_body)
+    str1 = res["result"].split("\n")
+    # for x in str1:
+    #     print(x)
+    data = []
+    str2=str1[27].split(" ")
+    for y in str2:
+        if(y!=""):
+            data.append(y)
+    json_body = [
+        {
+            "measurement": "GetProcSwaps",
+
+            "tags": {
+                # the address of lepd
+                "server": lepdClient.server
+            },
+            # "time": "2017-03-12T22:00:00Z",
+            "fields": {
+                "CPU": float(data[1]),
+                "HI/s": float(data[2]),
+                "TIMER/s": float(data[3]),
+                "NET_TX/s": float(data[4]),
+                "NET_RX/s": float(data[5]),
+                "BLOCK/s": float(data[6]),
+                "IRQ_POLL/s": float(data[7]),
+                "TASKLET/s": float(data[8]),
+                "SCHED/s": float(data[9]),
+                "HRTIMER/s": float(data[10]),
+                "RCU/s": float(data[11])
+            }
+        }
+    ]
+
+    influxDbClient.write_points(json_body)
 
 
 
