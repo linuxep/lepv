a2477e657ac9d0a7eead3202a9efabf63e09f342
lixusheng <1530830954@qq.com>
2018-05-21 11:33:18 +0800

    同时请求，保存CQ

--- dataStore/modules/run.py
+++ dataStore/modules/run.py
@@ -6,6 +6,7 @@ from dataStore.influxDbUtil.dbUtil import MyInfluxDbClient
 from dataStore.modules.io.pullAndStoreGetCmdIotop import pullAndStoreGetCmdIotop
 from dataStore.modules.cpu.pullAndStoreGetCmdMpstat import pullAndStoreGetCmdMpstat
 from dataStore.modules.memory.pullAndStoreGetProcMeminfo import pullAndStoreGetProcMeminfo
+from dataStore.modules.io.pullAndStoreGetCmdIostat import pullAndStoreGetCmdIostat
 
 import time
 
@@ -13,8 +14,9 @@ import time
 if (__name__ == '__main__'):
     lepdClient = LepdClient('localhost')
     influxDbClient = MyInfluxDbClient('localhost')
-    for i in range(60):
+    for i in range(120):
         pullAndStoreGetCmdIotop(lepdClient, influxDbClient)
+        pullAndStoreGetCmdIostat(lepdClient,influxDbClient)
         pullAndStoreGetProcMeminfo(lepdClient,influxDbClient)
         pullAndStoreGetCmdMpstat(lepdClient,influxDbClient)
         time.sleep(1)
