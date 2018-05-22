6fac70143e75734eafa5692c579083521dc98822
lixusheng <1530830954@qq.com>
2018-05-22 12:30:36 +0800

    完成解析存储

--- dataStore/modules/pullAndStoreGetCmdTop.py
+++ dataStore/modules/pullAndStoreGetCmdTop.py
@@ -12,25 +12,25 @@ store the returned data into the influxDB by influxDBClient.
 '''
 def pullAndStoreGetCmdTop(lepdClient, influxDbClient):
     res = lepdClient.sendRequest('GetCmdTop')
-    print(res)
+    # print(res)
+    # str1 = res["result"].split("\n")
+    # for x in str1:
+    #     print(x)
+    json_body = [
+        {
+            "measurement": "GetCmdTop",
+            "tags": {
+                # the address of lepd
+                "server": lepdClient.server
+            },
+            # "time": "2017-03-12T22:00:00Z",
+            "fields": {
+                "top": res["result"]
+            }
+        }
+    ]
 
-    # json_body = [
-    #     {
-    #         "measurement": "GetProcSwaps",
-    #         "tags": {
-    #             # the address of lepd
-    #             "server": lepdClient.server
-    #         },
-    #         # "time": "2017-03-12T22:00:00Z",
-    #         "fields": {
-    #             "LinuxVersion": mystr,
-    #             "compact_stall": int(data['compact_stall']),
-    #             "balloon_migrate": int(data['balloon_migrate']),
-    #         }
-    #     }
-    # ]
-    #
-    # influxDbClient.write_points(json_body)
+    influxDbClient.write_points(json_body)
 
 
 if (__name__ == '__main__'):
