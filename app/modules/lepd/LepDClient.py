59e2b883fe1b6dbc90db79bc870104a9cdbc774b
joyz <yudi4@163.com>
2019-07-12 23:28:19 +0800

    modify

--- app/modules/lepd/LepDClient.py
+++ app/modules/lepd/LepDClient.py
@@ -1,14 +1,14 @@
+import click
+import datetime
+import re
+import pprint
+from decimal import Decimal
+import socket
+import json
 """Core module for interacting with LEPD"""
-__author__    = "Copyright (c) 2016, Mac Xu <shinyxxn@hotmail.com>"
+__author__ = "Copyright (c) 2016, Mac Xu <shinyxxn@hotmail.com>"
 __copyright__ = "Licensed under GPLv2 or later."
 
-import json
-import socket
-from decimal import Decimal
-import pprint
-import re
-import datetime
-import click
 
 class LepDClient:
 
@@ -17,7 +17,8 @@ class LepDClient:
         self.port = port
         self.bufferSize = 2048
         self.config = config
-        
+        self.sock = None
+
         self.LEPDENDINGSTRING = 'lepdendstring'
 
     def listAllMethods(self):
@@ -31,7 +32,7 @@ class LepDClient:
     def ping(self):
         # print('Send "SayHello" command to LEPD')
         response = self.sendRequest("SayHello")
-            
+
         # print(response)
         if (response != None and 'result' in response and response['result'].startswith('Hello')):
             return True
@@ -42,11 +43,10 @@ class LepDClient:
     # Decimal is not a data type supported by JSON
     def toDecimal(self, val, precision='0.00'):
         try:
-            return float(val) #Decimal(val).quantize(Decimal(precision))
+            return float(val)  # Decimal(val).quantize(Decimal(precision))
         except Exception as err:
             print(err)
             return float(0)
-                                        
 
     def getIostatResult(self):
         response = self.sendRequest("GetCmdIostat")
@@ -60,18 +60,18 @@ class LepDClient:
         for i in range(len(resultLines)):
             if (not resultLines[i].startswith('Device:')):
                 continue
-            
+
             return resultLines[i:]
 
     def tryAllMethods(self):
         methods = self.listAllMethods()
-        
+
         executionResults = {}
         for methodName in methods:
 
             print('')
             print('<[ ' + methodName + " ]>")
-            
+
             executionResults[methodName] = {}
 
             startTime = datetime.datetime.now()
@@ -79,7 +79,7 @@ class LepDClient:
             endTime = datetime.datetime.now()
             executionResults[methodName]['duration'] = "%.1f" % ((endTime - startTime).total_seconds())
             print('duration:=' + executionResults[methodName]['duration'])
-            
+
             if (response == None or 'result' not in response):
                 executionResults[methodName]['return'] = None
                 print('Return:= Failed!')
@@ -88,57 +88,55 @@ class LepDClient:
                 executionResults[methodName]['return'] = lines
                 for line in lines:
                     print(line)
-        
-        
+
         print("")
         print("Summary:")
 
         for methodName, executionResult in executionResults.items():
             resultSumamry = "[" + methodName + "]("
             if (executionResult['return'] == None):
-                resultSumamry += "Failed) in " 
+                resultSumamry += "Failed) in "
             else:
                 resultSumamry += "Succeeded) in "
-            
+
             resultSumamry += executionResult['duration'] + ' seconds'
             print(resultSumamry)
-        
+
         return executionResults
 
     def getUnitTestResponse(self, commandName, arch='arm'):
-        
+
         currentDir = os.path.dirname(os.path.realpath(__file__))
         jsonFilePath = os.path.join(currentDir, 'tests', commandName, 'raw', arch + ".txt")
-        
+
         with open(jsonFilePath) as data_file:
             return json.load(data_file)
-    
+
     def getSystemInfo(self):
         responseLines = self.getResponse('GetProcVersion')
         if (len(responseLines) == 0):
             return {}
-        
+
         # it has just one line
         # a line like this:
         # Linux version 3.13.0-86-generic (buildd@lgw01-19) (gcc version 4.8.2 (Ubuntu 4.8.2-19ubuntu1) ) #130-Ubuntu SMP Mon Apr 18 18:27:15 UTC 2016
         responseLine = responseLines.pop(0)
-        
+
         sysInfo = {}
         sysInfo['os'] = 'linux'
         sysInfo['kernel'] = '3.13.0-86-generic'
         sysInfo['gcc'] = '4.8.2'
         sysInfo['distribution'] = 'ubuntu'
         sysInfo['version'] = '4.8.2'
-        
+
         return sysInfo
-        
-    
+
     def getResponse(self, methodName):
         if (self.config != 'unittest'):
             response = self.sendRequest(methodName)
         else:
             response = self.getUnitTestResponse(methodName)
-            
+
         if (response == None or 'result' not in response):
             return []
 
@@ -147,18 +145,26 @@ class LepDClient:
 
     def split_to_lines(self, longString):
         return re.split(r'\\n|\n', longString.strip())
-        
-    def sendRequest(self, methodName):
-        sock = None
 
+    def connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # initialise our socket
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # initialise our socket
-            sock.connect((self.server, self.port)) # connect to host <HOST> to port <PORT>
+            sock.connect((self.server, self.port))  # connect to host <HOST> to port <PORT>
+        except Exception as e:
+            print("conncet error: ", e)
+            return None
+        self.sock = sock
 
+    def sendRequest(self, methodName):
+        while(self.sock is None):
+            self.connect()
+
+        sock = self.sock
+        try:
             input_data = {}
             input_data['method'] = methodName
 
-            dumped_data = json.dumps(input_data) # Dump the input dictionary to proper json
+            dumped_data = json.dumps(input_data)  # Dump the input dictionary to proper json
 
             sock.send(dumped_data.encode())
             serverResponse = str.encode("")
@@ -166,22 +172,21 @@ class LepDClient:
             while True:
                 data = sock.recv(self.bufferSize)
                 if end in data:
-                    data = data.replace(end,str.encode(""))
+                    data = data.replace(end, str.encode(""))
                     serverResponse = serverResponse + data
                     break
                 serverResponse = serverResponse + data
-            responseJsonDecoded = json.loads(serverResponse.decode()) # decode the data received
+            responseJsonDecoded = json.loads(serverResponse.decode())  # decode the data received
 
             return responseJsonDecoded
-
-        except Exception as error:
-            pass
+        except socket.error as err:
+            print("connect lost ", err, "  try reconnect")
+            self.sock = None
+            self.sendRequest(methodName)
             # print(methodName + ": " + str(error))
             # if (error.strerror == 'nodename nor servname provided, or not known'):
             #     print('please double check the server to monitor is reachable, and the method is supported by LEPD')
-        finally:
-            if (sock):
-                sock.close()
+
 
 @click.command()
 @click.option('--server', default='www.rmlink.cn', help='Lepd server')
@@ -189,7 +194,7 @@ class LepDClient:
 @click.option('--config', default='debug', help='config setting')
 @click.option('--out', default=None, help='Log output file')
 def main(server, method, config, out):
-    
+
     # MEMO:
     # procrank is to replace smem, ( smem will retire )
     # iopp is to replace iotop, ( iotop is to retire )
@@ -197,7 +202,7 @@ def main(server, method, config, out):
 
     pp = pprint.PrettyPrinter(indent=2)
     client = LepDClient(server, config=config)
-    
+
     # pp.pprint(client.getSystemInfo())
     func = getattr(client, method)
     results = func()
@@ -211,6 +216,6 @@ def main(server, method, config, out):
     # pp.pprint(client.getResponse('GetCmdDf'))
 
 
-if( __name__ =='__main__' ):
+if(__name__ == '__main__'):
     main()
     sys.exit(0)
